e839e3cc5edc0a9758201b2b2ee44d3728880a0e
snguyen5147 <snguyen5001@gmail.com>
2026-08-27 22:54:10 -0600

    added lanes

--- src/Simulator/GUIMain.java
+++ src/Simulator/GUIMain.java
@@ -1,17 +1,11 @@
 package Simulator;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Platform;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+
 import java.util.ArrayList;
 
 
@@ -23,11 +17,22 @@ public class GUIMain{
     private static final double WINDOW_WIDTH = 1200;
     private static final double WINDOW_HEIGHT = 700;
 
+    // Distance between dashes
+    private static final double DASH_LENGTH = 30;
+    private static final double GAP_LENGTH = 20;
+
     //road dimensions
     private static final int LANES_PER_DIRECTION = 3;
     private static final int LANE_WIDTH = 40;
 
     private static final double ROAD_WIDTH = LANES_PER_DIRECTION * 2 * LANE_WIDTH;
+
+    //distance from intersection where line begins
+    private static final double LINE_LENGTH = 100;
+
+    //crosswalk
+    private static final double CROSSWALK_WIDTH = 50;
+    private static final double CROSSWALK_GAP = 10;
 
     private final ArrayList<GUILane> LaneList = new ArrayList<>();
     private final ArrayList<GUICar> CarList = new ArrayList<>();
@@ -61,6 +66,13 @@ public class GUIMain{
     private void createIntersection() {
         drawHorizontalRoad();
         drawVerticalRoad();
+
+        drawHorizontalLanes();
+        drawVerticalLanes();
+
+        //drawStopLines();
+
+        //drawCrosswalkss();
     }
 
     //horizontal road
@@ -83,6 +95,127 @@ public class GUIMain{
         verticalRoad.setFill(Color.DARKGRAY);
 
         streetPane.getChildren().add(verticalRoad);
+    }
+
+    //draw horizontal lanes
+    private void drawHorizontalLanes() {
+        double horizontalRoad = (WINDOW_HEIGHT - ROAD_WIDTH) / 2; //where the lane starts
+
+        double intersectionLeft = (WINDOW_WIDTH - ROAD_WIDTH) / 2; //left edge of the road
+
+        double intersectionRight = intersectionLeft + ROAD_WIDTH; //right edge of the road
+
+        //draw lanes
+        //6 lanes total; 5 lines
+        for(int i = 1; i < LANES_PER_DIRECTION * 2; i++) {
+            double y = horizontalRoad + i * LANE_WIDTH; //calculates the y coordinate of the line
+
+            //center divider
+            if(i == LANES_PER_DIRECTION) {
+                drawHorizontalCenterDivider(y, Color.YELLOW, 0, intersectionLeft - LINE_LENGTH);
+                drawHorizontalCenterDivider(y, Color.YELLOW, intersectionRight + LINE_LENGTH, WINDOW_WIDTH);
+
+                continue;
+            }
+
+            //dashed line left side
+            drawHorizontalDashedLine(y, 0, intersectionLeft - LINE_LENGTH);
+
+            //dashed line right side
+            drawHorizontalDashedLine(y, intersectionRight + LINE_LENGTH, WINDOW_WIDTH);
+        }
+    }
+
+
+    //horizontal dashed line
+    private void drawHorizontalDashedLine(double y, double startX, double endX) {
+
+        //draw dashed lines until endX
+        for (double x = startX; x < endX; x += DASH_LENGTH + GAP_LENGTH) {
+
+            double width = Math.min(DASH_LENGTH, endX - x);
+
+            if (width <= 0) {
+                break;
+            }
+
+            Rectangle dash = new Rectangle(x, y, width, 2);
+
+            dash.setFill(Color.WHITE);
+
+            streetPane.getChildren().add(dash);
+        }
+    }
+
+
+    //solid horizontal line
+    private void drawHorizontalCenterDivider(double y, Color color, double startX, double endX) {
+
+        Rectangle line = new Rectangle(startX, y, endX - startX, 2);
+
+        line.setFill(color);
+
+        streetPane.getChildren().add(line);
+    }
+
+    //draw horizontal lanes
+    private void drawVerticalLanes() {
+        double verticalRoad = (WINDOW_WIDTH - ROAD_WIDTH) / 2; //where the lane starts
+
+        double intersectionTop = (WINDOW_HEIGHT - ROAD_WIDTH) / 2; //top side of the road
+
+        double intersectionBottom = intersectionTop + ROAD_WIDTH; //bottom side of the road
+
+        //draw lanes
+        for(int i = 1; i < LANES_PER_DIRECTION * 2; i++) {
+            double x = verticalRoad + i * LANE_WIDTH;
+
+            //center divider
+            if(i == LANES_PER_DIRECTION) {
+                drawVerticalCenterDivider(x, Color.YELLOW, 0, intersectionTop - LINE_LENGTH);
+                drawVerticalCenterDivider(x, Color.YELLOW, intersectionBottom + LINE_LENGTH, WINDOW_HEIGHT);
+
+                continue;
+            }
+
+            //dashed line left side
+            drawVerticalDashedLine(x, 0, intersectionTop - LINE_LENGTH);
+
+            //dashed line right side
+            drawVerticalDashedLine(x, intersectionBottom + LINE_LENGTH, WINDOW_HEIGHT);
+        }
+    }
+
+
+    //horizontal dashed line
+    private void drawVerticalDashedLine(double x, double startY, double endY) {
+
+        //draw dashed lines
+        for (double y = startY; y < endY; y += DASH_LENGTH + GAP_LENGTH) {
+
+            double height = Math.min(DASH_LENGTH, endY - y);
+
+            if (height <= 0) {
+                break;
+            }
+
+            Rectangle dash = new Rectangle(x, y, 2, height);
+
+            dash.setFill(Color.WHITE);
+
+            streetPane.getChildren().add(dash);
+        }
+    }
+
+
+    //solid horizontal line
+    private void drawVerticalCenterDivider(double x, Color color, double startY, double endY) {
+
+        Rectangle line = new Rectangle(x, startY, 2, endY - startY);
+
+        line.setFill(color);
+
+        streetPane.getChildren().add(line);
     }
 
 

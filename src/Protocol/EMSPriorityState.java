1b87bf3f8be64c953a2124905d4fcbf28f4d9647
Swastika Dawadi <swastikadawadi5865@gmail.com>
2026-09-09 18:54:02 -0600

    Added EMSPriorityState enum

---
+++ src/Protocol/EMSPriorityState.java
@@ -0,0 +1,10 @@
+package Protocol;
+
+/**
+ * State of an EMS priority request from the antenna receiver.
+ * Carried as the `value` field of an EMS_PRIORITY_REQUEST / EMS_PRIORITY_CANCEL SimulatorEvent.
+ */
+public enum EMSPriorityState {
+    REQUEST,
+    CANCEL
+}

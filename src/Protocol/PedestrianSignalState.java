dfe47bb625da778e9169bde3ac6e17ff367fe67b
Swastika Dawadi <swastikadawadi5865@gmail.com>
2026-09-09 18:54:19 -0600

    Add PedestrianSignalState enum

---
+++ src/Protocol/PedestrianSignalState.java
@@ -0,0 +1,10 @@
+package Protocol;
+
+/**
+ * State of a pedestrian crossing signal.
+ * Carried as the `value` field of an UPDATE_PEDESTRIAN_SIGNAL SimulatorEvent.
+ */
+public enum PedestrianSignalState {
+    WALK,
+    WAIT
+}

3c3fa1be2886f29096d8c86c526aaabadbf9bac4
snguyen5147 <snguyen5001@gmail.com>
2026-09-24 17:05:52 -0600

    fixed timer logic in PedestrianMode

--- src/Harness/PedestrianMode.java
+++ src/Harness/PedestrianMode.java
@@ -3,20 +3,18 @@ package Harness;
 import Communication.PedestrianSignalState;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class PedestrianMode {
-    private Pedestrian pedestrian;
+    private final Pedestrian pedestrian;
     private final long duration = 15_000;
     private TrafficLights trafficLights;
-    private Timer timer;
-
-    private long startTime; //time when walk started
+    private final Timer timer;
 
     public PedestrianMode(
             Pedestrian pedestrian,
             TrafficLights trafficLights, Timer timer) {
         this.pedestrian = pedestrian;
-        this.startTime = 0;
         this.trafficLights = trafficLights;
         this.timer = timer;
     }
@@ -26,19 +24,13 @@ public class PedestrianMode {
             //walk
             pedestrian.pedWalk();
 
-            //set timer
-            startTime = System.currentTimeMillis();
-        }
-    }
+            timer.schedule(new TimerTask() {
 
-    //update timer
-    public void update() {
-        if(pedestrian.getState() == PedestrianSignalState.WALK) {
-            long elasped = System.currentTimeMillis() - startTime;
-
-            if(elasped >= duration) {
-                timeout();
-            }
+                @Override
+                public void run() {
+                    timeout();
+                }
+            }, duration);
         }
     }
 
@@ -46,14 +38,11 @@ public class PedestrianMode {
         if(pedestrian.getState() == PedestrianSignalState.WALK) {
             //stop
             pedestrian.pedStop();
-
-            startTime = 0;
         }
     }
 
     public void reset() {
         pedestrian.pedStop();
-        startTime = 0;
     }
 
     public PedestrianSignalState getState() {
